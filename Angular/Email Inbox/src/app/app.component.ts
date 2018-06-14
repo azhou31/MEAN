251d3837ee025acecc370fcd086cbb7a7b219f2b
Angie Zhou <angelazhou.23@gmail.com>
2018-06-13 18:12:58 -0700

    Added files without node modules files

---
+++ Angular/Email Inbox/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  emails =[
+    {email:'Bill@gates.com', important:true, subject:"New Windows", content: 'Windows XI will launch in year 2100'},
+    {email:'ADA@lovelace.com', important:true, subject:"Programming", content: 'Enchantress of Numbers'},
+    {email:'John@carmac.com', important:false, subject:"Updated Algo", content: 'New algorithm for shadow volumes'},
+    {email:'Gabe@newel.com', important:false, subject:"HL3!", content: 'Just kidding...'},
+  ]
+}
